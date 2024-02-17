d4acbe1addd3ff0048140e4a84b346887fcfd9cf
nasir.rashid_isteer <nasirganie193201@gmail.com>
2024-02-17 09:26:23 +0530

    fist commit to master

---
+++ src/test/java/com/isteer/dcm/DcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isteer.dcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
